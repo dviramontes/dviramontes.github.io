bc80d675e0398a897824993916f78d2c4f254f3a
david viramontes <dviramontes@gmail.com>
2014-10-19 23:40:17 -0600

    adds thumbnail.view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+var Gallery = Gallery || {}; // ns
+
 //(function(global){
 
 "use strict";
@@ -5,20 +7,27 @@
 var key = 'XtnjbBbOkULcb7ePVhDNvEEjpCvx7vIm';
 var endpoint = 'http://www.behance.net/v2/users/dreampilot';
 
-var Gallery = {}; // ns
+
 
 Gallery.projectList = Array; // collection
 
 Gallery.Project = function (data) {
-	this.name = data.name;
+	this.name = m.prop(data.name);
+	this.thumbnail = m.prop(data.thumbnail);
+	this.fields = m.prop(data.fields);
 };
 
 // Gallery View-Model
 Gallery.viewModel = {};
 Gallery.viewModel.init = function () {
 	this.list = new Gallery.projectList();
-	this.add = function (name) {
-		this.list.push(new Gallery.Project({"name": name}));
+	this.add = function (obj) {
+		this.list
+			.push(new Gallery.Project({
+				"name": obj.name,
+				"thumbnail": obj.covers['202'],
+				"fields" : obj.fields
+			}));
 	}.bind(this);
 }
 
@@ -29,19 +38,56 @@ Gallery.controller = function () {
 	Gallery.viewModel.init();
 }
 
-// list view
+// views
 Gallery.listView = function () {
-	return m('div#listview',[
+	return m('div#list-view',[
 		m('ul', [
 			Gallery.viewModel.list.map(function (project, index) {
 				return m('li', [
-					m('a[href=#' + project.name + ']' , project.name)
+					m('a[href=#' + project.name() + ']' , project.name())
 				])
 			})
 		])
 	]);
+};
 
-}
+//<div class="row">
+//	<div class="col-xs-6 col-md-3">
+//		<a href="#" class="thumbnail">
+//			<img data-src="holder.js/100%x180" alt="...">
+//			</a>
+//			<div class="caption">
+//				<h3>Thumbnail label</h3>
+//				<p>...</p>
+//				<p></p>
+//			</div>
+//		</div>
+//	</div>
+
+Gallery.thumbnailView = function () {
+	return m('div#thumbnail-view',[
+		m('div', [
+			Gallery.viewModel.list.map(function (project, index) {
+				return m('div', {class:'col-xs-6 col-md-3 col-sm-6'},[
+					m('a',{class:'thumbnail',href:'#' + project.name()},[
+						m('img', {'src' : project.thumbnail()}),
+						m('div',{class:'my-caption'}, [
+							m('p', [
+								m('span',project.name()),
+								m('br'),
+								m('span',[
+									project.fields().map(function(p){
+										return m('em', p)
+									})
+								])
+							])
+						])
+					])
+				])
+			})
+		])
+	]);
+};
 
 //$.ajax(endpoint + '/projects?' + 'api_key=' + key, {
 $.ajax('./scripts/projects.json', {
@@ -54,19 +100,29 @@ $.ajax('./scripts/projects.json', {
 
 function withData(data) { // process jsonp callback
 	console.info(data['http_code']);
+	//console.debug(data.projects);
 	data.projects.map(function (e) {
-		console.log(e.name)
-		Gallery.viewModel.add(e.name);
+		console.log(e)
+		Gallery.viewModel.add(e);
 	});
 
-	//Gallery.viewModel.init();
-
 	console.log(Gallery.viewModel.list.length)
+	//console.log(Gallery.viewModel.list)
 
-	m.render(document.body.querySelector('#listview'), Gallery.listView());
+	m.render(
+		document
+			.querySelector('#list-view'), Gallery.listView()
+	);
+
+	m.render(
+		document
+			.querySelector('#thumbnail-view'), Gallery.thumbnailView()
+	);
+
+
+	// init app
 	//m.module(document, Gallery);
 }
-
 
 //})(this);
 
